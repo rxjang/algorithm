eff19e9423bab250ab18497c4c5858d2fe643b52
장서현 <rxforp@naver.com>
2021-07-02 00:12:10 +0900

    queue

---
+++ chap04/IntArrayQueue.java
@@ -0,0 +1,79 @@
+package chap04;
+
+public class IntArrayQueue {
+    private int max;    // 큐 용량
+    private int num;    // 현재 데이터 수
+    private int[] que;   // 큐 본체
+
+    public static class EmptyIntArrayQueException extends RuntimeException {
+        public EmptyIntArrayQueException() {}
+    }
+
+    public static class OverflowIntArrayQueException extends RuntimeException {
+        public OverflowIntArrayQueException() {}
+    }
+
+    public IntArrayQueue(int capacity) {
+        num = 0;
+        max = capacity;
+        try {
+            que = new int[capacity];
+        } catch (OutOfMemoryError e) {
+            max = 0;
+        }
+    }
+
+    public int enqueue(int x) throws OverflowIntArrayQueException {
+        if(num >= max) {
+            throw new OverflowIntArrayQueException();
+        }
+        return que[num++] = x;
+    }
+
+    public int dequeue() throws EmptyIntArrayQueException {
+        if(num <= 0) {
+            throw new EmptyIntArrayQueException();
+        }
+        int value = que[0];
+        for(int i = 0; i < num - 1; i++) {
+            que[i] = que[i + 1];
+        }
+        num --;
+        return value;
+    }
+
+    public int front() throws EmptyIntArrayQueException {
+        if(num <= 0) {
+            throw new EmptyIntArrayQueException();
+        }
+        return que[0];
+    }
+
+    public int rear() throws EmptyIntArrayQueException {
+        if(num <= 0) {
+            throw new EmptyIntArrayQueException();
+        }
+        return que[num-1];
+    }
+
+    public void dump() throws EmptyIntArrayQueException {
+        if(num <= 0) {
+            throw new EmptyIntArrayQueException();
+        }
+        for(int i = 0; i < num; i++) {
+            System.out.print(que[i] + " ");
+        }
+    }
+
+    public void clear() {
+        num = 0;
+    }
+
+    public int size() {
+        return num;
+    }
+
+    public int capacity() {
+        return max;
+    }
+}
---
+++ chap04/IntArrayQueueTester.java
@@ -0,0 +1,69 @@
+package chap04;
+
+import java.util.Scanner;
+
+public class IntArrayQueueTester {
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+        IntArrayQueue s = new IntArrayQueue(10);
+
+        while(true) {
+            System.out.println("현재 데이터 수: " + s.size() + " / " + s.capacity());
+            System.out.print("(1)인큐 (2)디큐 (3)프론트 (4)리어 (5)덤프 (6)clear (0)종료 : ");
+            int menu = stdIn.nextInt();
+            if(menu == 0) break;
+
+            int x;
+            switch (menu) {
+                case 1:
+                    // 인큐
+                    System.out.print("데이터: ");
+                    x = stdIn.nextInt();
+                    try{
+                        s.enqueue(x);
+                    } catch (IntArrayQueue.OverflowIntArrayQueException e) {
+                        System.out.println("큐가 가득 찼습니다.");
+                    }
+                    break;
+
+                case 2:
+                    // 디큐
+                    try{
+                        x = s.dequeue();
+                        System.out.println("디큐한 데이터는 "+ x + "입니다.");
+                    } catch (IntArrayQueue.EmptyIntArrayQueException e) {
+                        System.out.println("큐가 비어 있습니다.");
+                    }
+                    break;
+                case 3:
+                    // 프론트
+                    try {
+                        x = s.front();
+                        System.out.println("프론트 데이터는 " + x + "입니다.");
+                    } catch (IntArrayQueue.EmptyIntArrayQueException e ) {
+                        System.out.println("큐가 비어 있습니다.");
+                    }
+                    break;
+                case 4:
+                    x = s.rear();
+                    try {
+                        System.out.println("리어 데이터는 " + x + "입니다.");
+                    } catch (IntArrayQueue.EmptyIntArrayQueException e){
+                        System.out.println("큐가 비어 있습니다.");
+                    }
+                    break;
+                case 5:
+                    try {
+                        s.dump();
+                    } catch (IntArrayQueue.EmptyIntArrayQueException e){
+                        System.out.println("큐가 비어 있습니다.");
+                    }
+                    break;
+                case 6:
+                    s.clear();
+                    break;
+
+            }
+        }
+    }
+}
