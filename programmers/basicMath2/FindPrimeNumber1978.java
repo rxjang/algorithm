b21026f4f919df5830da91e3896e7cefbd64bb19
장서현 <olllehyun@gmail.com>
2021-12-28 08:51:25 +0900

    prime number algorithm

---
+++ programmers/basicMath2/FindPrimeNumber1978.java
@@ -0,0 +1,52 @@
+package programmers.basicMath2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class FindPrimeNumber1978 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int answer = 0;
+
+        while(st.hasMoreTokens()) {
+            int value = Integer.parseInt(st.nextToken());
+
+            if(value == 2 || value == 3 || value == 5 || value == 7) {
+                answer ++;
+                continue;
+            }
+
+            if(value <= 10 || value % 2 == 0) continue;
+
+            boolean primeNumber = true;
+            for(int i = 3; i < value / 2; i += 2) {
+                if(value % i == 0)  {
+                    primeNumber = false;
+                    break;
+                }
+            }
+            if(primeNumber) answer ++;
+        }
+
+        System.out.println(answer);
+    }
+
+    // 다른 방법
+    boolean isPrimeNumber(int value) {
+        if(value == 1) {
+            return false;
+        }
+
+        // 제곱근 보다 큰 수는 검사할 필요가 없음 (제곱근 이상의 수 끼리 곱하면 해당 수를 넘어버리기 때문)
+        for(int i = 2; i <= Math.sqrt(value); i++) {
+            if(value % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ programmers/basicMath2/PrimeNumber2581.java
@@ -0,0 +1,42 @@
+package programmers.basicMath2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class PrimeNumber2581 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int start = Integer.parseInt(br.readLine());
+        int end = Integer.parseInt(br.readLine());
+
+        boolean[] arr = new boolean[end - start + 1];
+
+        for(int i = start; i<= end; i++) {
+            if(i == 1) continue;
+            if(i !=2 && i % 2 == 0) continue;
+
+            boolean check = true;
+            for(int j = 2; j <= Math.sqrt(i); j++) {
+                if(i % j == 0) {
+                    check = false;
+                    break;
+                }
+            }
+            arr[i-start] = check;
+        }
+
+        int min = 0;
+        int sum = 0;
+        for(int i = 0; i < arr.length; i++) {
+            if(min == 0 && arr[i]) min = (i + start);
+            if(arr[i]) sum += (i + start);
+        }
+
+        if(sum == 0) {
+            System.out.println(-1);
+        } else {
+            System.out.printf("%d\n%d%n", sum, min);
+        }
+    }
+}
