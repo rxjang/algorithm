e667db9395ccc037e41778fe11736d619d5c37d1
장서현 <olllehyun@gmail.com>
2021-12-31 21:22:51 +0900

    직사각형 관련 알고리즘

---
+++ programmers/basicMath2/CETVRTA3009.java
@@ -0,0 +1,28 @@
+package programmers.basicMath2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class CETVRTA3009 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int[] arrX = new int[3];
+        int[] arrY = new int[3];
+
+        for(int i = 0; i < 3; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            arrX[i] = Integer.parseInt(st.nextToken());
+            arrY[i] = Integer.parseInt(st.nextToken());
+        }
+
+        System.out.printf("%d %d",
+                arrX[0] == arrX[1] ? arrX[2]
+                        : arrX[1] == arrX[2] ? arrX[0] : arrX[1],
+                arrY[0] == arrY[1] ? arrY[2]
+                        : arrY[1] == arrY[2] ? arrY[0] : arrY[1]);
+    }
+}
---
+++ programmers/basicMath2/EscapeFromRectangle1085.java
@@ -0,0 +1,25 @@
+package programmers.basicMath2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class EscapeFromRectangle1085 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+        int w = Integer.parseInt(st.nextToken());
+        int h = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[]{x, y, w-x, h-y};
+        Arrays.sort(arr);
+
+        System.out.println(arr[0]);
+
+    }
+}
